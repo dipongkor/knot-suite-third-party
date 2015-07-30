2305920281c83f36e412848903bcab354c5b595e
dipongkor <atish@bs-23.com>
2015-07-30 13:37:14 +0600

    Repo scope change

--- routes/git.js
+++ routes/git.js
@@ -159,8 +159,11 @@ router.post("/getAllRepos", function (req, res, next) {
                 if (err) {
                     console.log(err);
                 }
-              //  console.log(data);
-                _(user.connectedRepositories).forEach(function(repo){
+                console.log(user.connectedRepositories);
+
+
+
+                user.connectedRepositories.forEach(function(repo){
                    var connectedRepo = _.find(data,{id:repo.id});
                     var connectedRepoIndex = data.indexOf(connectedRepo);
                     data.splice(connectedRepoIndex,1);
